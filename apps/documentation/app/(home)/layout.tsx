bfe5de7fd6516732000ed75faaf65fd4d0bac8d6
luc.le <trungluc.dev@gmail.com>
2026-01-06 21:35:39 +0700

    chore: update configuration and dependencies for documentation app
    - Refactor next.config.mjs to use double quotes for consistency.
    - Update package.json to include new scripts for formatting and linting, and add @biomejs/biome as a dev dependency.
    - Modify vercel.json to use consistent indentation.
    - Update bun.lock to reflect the new version of @biomejs/biome and adjust dependencies accordingly.
    - Update biome.json in the react package to set "root" to false.
    - Add a new biome.json configuration for the documentation app with specific linting and formatting rules.

--- apps/documentation/app/(home)/layout.tsx
+++ apps/documentation/app/(home)/layout.tsx
@@ -2,5 +2,5 @@ import { HomeLayout } from "fumadocs-ui/layouts/home";
 import { baseOptions } from "$/configs/layout.config";
 
 export default function Layout({ children }: LayoutProps<"/">) {
-  return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
+	return <HomeLayout {...baseOptions()}>{children}</HomeLayout>;
 }
--- apps/documentation/app/(home)/page.tsx
+++ apps/documentation/app/(home)/page.tsx
@@ -1,18 +1,19 @@
-import { IconsExplorer } from '$/components/icons-explorer';
-import { MetaData } from 'fumadocs-core/source';
-import { Suspense } from 'react';
+import { IconsExplorer } from "$/components/icons-explorer";
+import { MetaData } from "fumadocs-core/source";
+import { Suspense } from "react";
 
 export default function HomePage() {
-  return (
-    <div>
-      <Suspense fallback={<div>Loading...</div>}>
-        <IconsExplorer />
-      </Suspense>
-    </div>
-  );
+	return (
+		<div>
+			<Suspense fallback={<div>Loading...</div>}>
+				<IconsExplorer />
+			</Suspense>
+		</div>
+	);
 }
 
 export const metadata: MetaData = {
-  title: 'Icons Explorer - Solaris Icons',
-  description: 'Explore the wide variety of icons available in the Solaris Icons library.',
-}
+	title: "Icons Explorer - Solaris Icons",
+	description:
+		"Explore the wide variety of icons available in the Solaris Icons library.",
+};
